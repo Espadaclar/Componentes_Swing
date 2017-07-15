2deb69d1a8c9c6f4351b637d619583aa94f4d902
Espadaclar <eneroborde8002@gmail.com>
2017-07-16 00:49:36 +0200

    Comprueba si una direccion email es correcta vd 87.

--- JavaApplication33_MarcoTexto.java
+++ JavaApplication33_MarcoTexto.java
@@ -9,10 +9,9 @@ import javax.swing.*;
  * @author Usuario
  */
 public class JavaApplication33_MarcoTexto extends JFrame {
-
-    //almacena el texto que se escriva en el cuadro de texto.
+ //almacena el texto que se escriva en el cuadro de texto.
     private JTextField campo1;
-    
+
     public static void main(String[] args) {
         JavaApplication33_MarcoTexto marco = new JavaApplication33_MarcoTexto();
         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,8 +36,8 @@ public class JavaApplication33_MarcoTexto extends JFrame {
         public LaminaTexto2() {
             //asignamos el tipo de Layout a la lamina.
             setLayout(new BorderLayout());
-          LaminaTexto lamina = new LaminaTexto();
-          add(lamina, BorderLayout.NORTH);
+            LaminaTexto lamina = new LaminaTexto();
+            add(lamina, BorderLayout.NORTH);
 
         }
 
@@ -46,25 +45,42 @@ public class JavaApplication33_MarcoTexto extends JFrame {
 
             public LaminaTexto() {
                 //asinamos el tipo de Layout a la lamina.
-                setLayout(new GridLayout(1, 2, 193, 3));
+                setLayout(new GridLayout(1, 3, 93, 9));
 
-                campo1 = new JTextField( 20);
+                JLabel email = new JLabel("Direccion de correo");
+                add(email);
+                campo1 = new JTextField(20);
                 add(campo1);
-                JButton boton = new JButton("Recupera texto");
+                JButton boton = new JButton("Validar dirección");
                 add(boton);
                 OyenteTexto oyente = new OyenteTexto();
                 boton.addActionListener(oyente);
 
             }
-            
+
             //CLASE INTERNA PARA CREAR UN  OBJTO OYENTE
-            private class OyenteTexto implements ActionListener{
+            private class OyenteTexto implements ActionListener {
 
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    System.out.println(campo1.getText().trim());
+                    comprobarDireccionEmail();
                 }
-            
+
+                public void comprobarDireccionEmail() {
+                    String aux = campo1.getText();
+                    int cont = 0;
+                    char[] valor = aux.toCharArray();
+                    for (int i = 0; i < valor.length; i++) {
+                        if (valor[i] == '@') {
+                            cont++;
+                        }
+                    }
+                    if (cont == 0 || cont > 1) {
+                        System.out.println("Dirección incorrecta, fallan las @");
+                    } else {
+                        System.out.println("Direccion correcta");
+                    }
+                }
             }
         }
 
