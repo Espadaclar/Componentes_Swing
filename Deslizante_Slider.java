91f642e7791c17b884a53569a9a223c3b627bf0c
Espadaclar <eneroborde8002@gmail.com>
2017-07-21 15:37:31 +0200

    Ejercicio con Controles_Desilantes o JSliders. vd 97.

--- Deslizante_Slider.java
+++ Deslizante_Slider.java
@@ -1,7 +1,9 @@
 
 import java.awt.*;
-import javax.swing.*;
+import java.awt.event.*;
 
+import javax.swing.*;
+import javax.swing.event.*;
 /**
  *franciscoJavier
  */
@@ -11,15 +13,15 @@ public class Deslizante_Slider  extends JFrame {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-     Deslizante_Slider  marco = new Deslizante_Slider ();
+        Deslizante_Slider  marco = new Deslizante_Slider ();
         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public Deslizante_Slider() {
-        
+
         Lamina lamina = new Lamina();
         add(lamina);
-        
+
         setBounds(150, 70, 900, 500);
         setVisible(true);
         setTitle("Deslizantes_Sliders.");
@@ -30,26 +32,53 @@ public class Deslizante_Slider  extends JFrame {
     }
 
     public class Lamina extends JPanel {
+       
+        private JSlider desliza;
+        private JLabel texto;
+        private Font fuente;
+        
+        public Lamina(){
 
-        public Lamina() {
+
             setLayout(new BorderLayout());
             // --   PARA INDICAR LA ORIENTACION DEL Slaider EN EL CONS. UTILIZAR LA INTERFACE SwingConstants.
             // -- 2º para indica el comienzo, 3º para indica el final, 4º indica dode se situa el indicador.
-            JSlider desliza = new JSlider(SwingConstants.VERTICAL, 0, 50, 22);
+            desliza = new JSlider(SwingConstants.VERTICAL, 10, 50, 12);
             // -- CAMBIAMOS  LA FUENTE DEL Slider.
             desliza.setFont(new Font("Arial", Font.ITALIC, 12));
-            
+
             // --  PARA PONER MARCAS EN EL Slider, 1º se dice cada cuanto van
             desliza.setMajorTickSpacing(10);// --  MARCA MAYOR
             desliza.setMinorTickSpacing(2);// --  MARCA MAYOR
-            
+
             desliza.setPaintTicks(true);// ---  LE DECIMOS QUE IMPRIMA ESAS MARCAS.
             desliza.setPaintLabels(true);// --- LE DECIMOS QUE IMPRIMA LOS Nº.
             desliza.setSnapToTicks(true);// --- SI SOLTAMOS EL 'Slider' ENTRE DOS MARCAS SE VA AUTOMATICAMENTE
-                                         //---------A LA QUE MÁS CERCA LE QUEDE.
+            //---------A LA QUE MÁS CERCA LE QUEDE.
             JPanel laminaJs = new JPanel();
             laminaJs.add(desliza);
             add(laminaJs, BorderLayout.WEST);
+
+            texto = new JLabel();
+            texto.setText("Fe. La capacidad de cuestionamiento o pensamiento adecuado.");
+            fuente = new Font("Serif", Font.PLAIN, 13);
+            texto.setFont(fuente);
+            add(texto);
+
+            Oyente oyente = new Oyente();// -INSTANCIA  DE LA CL, OYENTE.
+            desliza.addChangeListener(oyente);
+        }
+
+        private class Oyente implements ChangeListener{
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int valor = desliza.getValue();
+                System.out.println(valor);
+                texto.setFont(new Font("Serif", Font.PLAIN, valor));
+            }
         }
     }
 }
+
+
