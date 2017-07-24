5b6b50fdaf13e2f6212a06857306d1c5e457c215
Espadaclar <eneroborde8002@gmail.com>
2017-07-24 22:06:37 +0200

    Afinada la funcionalidad del menu. vd 104 y 105.

--- Menu.java
+++ Menu.java
@@ -40,32 +40,31 @@ public class Menu extends JFrame
         Font fuente = new Font(null);//      da formato al texto
 
         public Lamina() {
-
-            setLayout(new BorderLayout());
+setLayout(new BorderLayout());
             add(laminaN, BorderLayout.NORTH);
             add(texto, BorderLayout.CENTER);
             //texto.setFont(new Font("Arial", Font.PLAIN, 22));
             laminaN.add(barraM);// -------------pone la barra dentro de una lamina.
-
+            
             fuent = new JMenu("Fuente");           
             stilo = new JMenu("Stilo");
             tamano = new JMenu("Tamaño");
-
+            
             barraM.add(fuent);
             barraM.add(stilo);
             barraM.add(tamano);
-            configuraMenu("Arial", "fuent", "Arial", Font.PLAIN, 15);
-            configuraMenu("Bernard MT Condensed", "fuent", "Bernard MT Condensed", Font.PLAIN, 15);
-            configuraMenu("Lucida Handwriting", "fuent", "Lucida Handwriting", Font.PLAIN, 15);
-
-            configuraMenu("Negrita", "stilo", "Bernard MT Condensed", Font.BOLD, 15);
-            configuraMenu("Normal", "stilo", "Lucida Handwriting", Font.PLAIN, 15);
-
-            configuraMenu("18", "tamano", "Arial", Font.PLAIN, 18);
-            configuraMenu("25", "tamano", "Bernard MT Condensed", Font.PLAIN, 25);
-            configuraMenu("10", "tamano", "Lucida Handwriting", Font.PLAIN, 10);
+            configuraMenu("Arial", "fuent", "Arial", 9, 1);// solo tiene que modificar el tipo de letra.
+            configuraMenu("Bell MT", "fuent", "Bell MT", 9, 1);
+            configuraMenu("Lucida Handwriting", "fuent", "Lucida Handwriting", 9, 1);
+            
+            configuraMenu("Negrita", "stilo", " ", Font.BOLD, 1);// solo tiene que modificar el stilo.
+            configuraMenu("Normal", "stilo", " ", Font.PLAIN, 1);
+            
+            configuraMenu("18", "tamano", " ", 9, 18);// solo tiene que modificar el tamano.
+            configuraMenu("25", "tamano", " ", 9, 25);//  el 9 e nada
+            configuraMenu("10", "tamano", " ", 9, 10);
         } 
-
+       
         /**
          * @param rotulo   ----nombre de subclase del elemento de la barra
          * @param menu    -----nombre del elemento de la barra
@@ -73,8 +72,8 @@ public class Menu extends JFrame
          * @param stilo      - Estilo que toma cada tipo de letra, negrita, cursiva etc
          * @param size       - Tamano de la letra. 
          */
-        public void configuraMenu(String rotulo, String menu, String nameFont, int stil, int size){
-
+        public void configuraMenu(String rotulo, String menu, String tipo_letra, int estilo, int tam){
+        
             JMenuItem elemento_menu = new JMenuItem(rotulo);
             // -- 'elemento_menu' SE ASINA A LA PESTANA DE LA BARRA ESPECIFICADA EN EL 2º PARAMETRO.
             if(menu == "fuent"){
@@ -84,12 +83,91 @@ public class Menu extends JFrame
             }else if(menu == "tamano"){
                 tamano.add(elemento_menu);
             }
-            elemento_menu.addActionListener(new ActionListener() { // ----- CLASE INTERNA ANONIMA.
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        texto.setFont(new Font(nameFont, stil, size));
-                    }
-                });                    
+            
+            //// la pasamos los mismos parametros con los que se crea el mt, y esta instancia 
+            // a su vez pasa el valor de sus atributos a la cl interna
+             elemento_menu.addActionListener(new Gestiona_Eventos(rotulo, tipo_letra, estilo, tam));                    
         }
+        
+        
+        // -------------- CLASE INTERNA PARA GESTIONAR LOS EVENTOS.
+        private class Gestiona_Eventos implements ActionListener{
+
+            
+            //1º tenemos que conseguir que los parametros del objeto 'Font' vallan cambiando en función
+            // del elemento de la barra que pulsemos y los otros se conserven. Para que se conserven 
+            //    declaramos atributos
+            private String tipo_texto; //----tipo de letra, Arial, Verdana...
+            private String menu; //-- el menu que pulsamos, Arial, Cursiva, 20, 14 ...
+            private int estilo_letra; // -- para especificar el estilo de letra con 'Font.PLAIN'
+            private int tamagno_letra; // --- para el tamaño de la letra
+            
+            /**
+             * ESTE CONS MAS QUE NADA ES PARA DETECTAR CUAL ES EL MENU EN EL QUE HEMOS PULSADO
+             * CUANDO INSTACIEMOS ESTA CL, EN LA CL PADRE,
+             * @param elemento --elemento que hemos pulsado, arial, negrita, 13, 23 ...
+             * @param texto2   --almacena el nombre del boton pulsado. Arial, Negrita ...
+             * @param estilo2  --almacena el estilo que hayamos pulsado en cada momento.
+             * @param tam_letra -almacen el tamano de la letra que hayamos pulsado. 
+             */
+            public Gestiona_Eventos(String elemento, String texto2, int estilo2, int tam_letra) {
+                
+                //4º damos valor a los atributos con el valor que demos a los parametros del cons
+                //   cuando instanciemos la cl en la cl padre.
+                tipo_texto = texto2;
+                menu = elemento;
+                estilo_letra = estilo2;
+                tamagno_letra = tam_letra;
+            }
+    
+            //2º sustituimos los parametros 'new Font("Arial", Font.PLAIN, 22)' por los atributos
+            // --------------MT PARA CAMBIAR EL TIPO DE LETRA.
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Font letra = texto.getFont();
+                
+                String valor = e.getActionCommand();
+
+                fuente = texto.getFont();
+                if (valor == "Negrita" || valor == "Normal") {
+                    tipo_texto = fuente.getFontName();
+                    tamagno_letra = fuente.getSize();
+                } else if (valor == "Lucida Handwriting" || valor == "Bell MT" || valor == "Arial") {
+                    tamagno_letra = fuente.getSize();
+                    estilo_letra = fuente.getStyle();
+                } else if (valor == "14" || valor == "18" || valor == "22" || valor == "28") {
+                    tipo_texto = fuente.getFontName();
+                    estilo_letra = fuente.getStyle();
+                }
+                
+                 texto.setFont(new Font(tipo_texto, estilo_letra, tamagno_letra));  
+                 System.out.println("TIPO. " +tipo_texto+ ".  -----TAMAÑO " +tamagno_letra+
+                                        ". ----ESTILO " +estilo_letra);
+            }
+            
+            //3º tenemos que almacenar en los atributos el menu correspondiente al tipo de texto
+            //   ,estilo de letra y tamano de letra.
+            //  tenemos que conseguir pasar los valores de la clase padre a la clase hijo, y para conseguir
+            // esto, utilizamos el constructor de la cl 'Gestiona_Eventos'
+            
+            //5º en el mt 'configuraMenu(....)' ponemos a la escucha a los objetos que vamos creando,
+            // y es hay donde instaciamos la cl Gestiona_Eventos 
+            //elemento_menu.addActionListener(new Gestiona_Eventos(rotulo, tipo_letra, estilo, tam));
+            // la pasamos los mismos parametros con los que se crea el mt
+        
+            
+            // 1, 2, 3 4,  y 5 EXPLICADO --> cuando para crear un objeto incocamos el mt
+            // 'configuraMenu("Arial", "fuent", "Arial", Font.PLAIN, 1);' cada uno de sus atri 
+            // viaja a los atri del mt; 'configuraMenu(String rotulo, String menu, 
+            //                                              String tipo_letra, int estilo, int tam)
+       
+            // de igual modo, 4 de estos atributos se almacenan en los param, que utilizamos
+            //        para instanciar al oyente, dentro del mismo mt..'configuraMenu("...)
+            // -----elemento_menu.addActionListener(new Gestiona_Eventos(rotulo, tipo_letra, estilo, tam));
+            //
+            // y el valor de estos parametros se almacena en los argumentos del cons de la clase
+            //  'Gestiona_Eventos(String elemento, String texto2, int estilo2, int tam_letra)' que a su vez
+            // le pasa el valor a sus atributos donde queda almacenado el valor
+        }     
     }
 }
