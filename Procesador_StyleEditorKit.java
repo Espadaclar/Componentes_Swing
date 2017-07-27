e763bbf97b5aba6a669a4630cdd08d490b09dbd2
Espadaclar <eneroborde8002@gmail.com>
2017-07-27 16:49:37 +0200

    CheckBox y RadioButton en las etiquetas. vd 108.

--- Procesador_StyleEditorKit.java
+++ Procesador_StyleEditorKit.java
@@ -39,9 +39,14 @@ public class Procesador_StyleEditorKit extends JFrame
         JTextPane texto = new JTextPane();// cuadro de texto
         Font fuente = new Font(null);//      da formato al texto
 
-        private int z;
+        private int z; // --almacena el tamano de la letra.
+        private ButtonGroup group;
+        private JRadioButtonMenuItem radio;
+        private ButtonGroup group2;
+        private JRadioButtonMenuItem radio2;
 
         public Lamina() {
+            group = new ButtonGroup();
             setLayout(new BorderLayout());
             add(laminaN, BorderLayout.NORTH);
             add(texto, BorderLayout.CENTER);
@@ -55,52 +60,88 @@ public class Procesador_StyleEditorKit extends JFrame
             barraM.add(fuent);
             barraM.add(stilo);
             barraM.add(tamano);
-            configuraMenu("Arial", "fuent", "Arial", 9, z, "../iconos/arial.gif");// solo tiene que modificar el tipo de letra.
+            /* configuraMenu("Arial", "fuent", "Arial", 9, z, "../iconos/arial.gif");// solo tiene que modificar el tipo de letra.
             configuraMenu("Bell MT", "fuent", "Bell MT", 9, z, "../iconos/bel.gif");
-            configuraMenu("Lucida Handwriting", "fuent", "Lucida Handwriting", 9, z, "../iconos/lucida.gif");
+            configuraMenu("Lucida Handwriting", "fuent", "Lucida Handwriting", 9, z, "../iconos/lucida.gif");*/
 
-            configuraMenu("Negrita", "stilo", " ", Font.BOLD, z, "");// solo tiene que modificar el stilo.
-            configuraMenu("Cursiva", "stilo", " ", Font.ITALIC, z, "");
+            creaFuenteJRadioButtonMenuItem("Arial", "../iconos/copiar.gif");
+            creaFuenteJRadioButtonMenuItem("Bell MT", "../iconos/copiar.gif");
+            creaFuenteJRadioButtonMenuItem("Lucida Handwriting", "../iconos/copiar.gif");
 
-            configuraMenu("18", "tamano", " ", 9, 18, "");
+            //configuraMenu("Negrita", "stilo", " ", Font.BOLD, z, "");// solo tiene que modificar el stilo.
+            //configuraMenu("Cursiva", "stilo", " ", Font.ITALIC, z, "");
+            JCheckBoxMenuItem negrita = new JCheckBoxMenuItem("Negrita", new ImageIcon("../iconos/azul.gif") );
+            JCheckBoxMenuItem cursiva = new JCheckBoxMenuItem("Cursiva", new ImageIcon("../iconos/amarillo.gif"));
+
+            negrita.addActionListener(new StyledEditorKit.BoldAction());
+            cursiva.addActionListener(new StyledEditorKit.ItalicAction());
+
+            stilo.add(negrita);
+            stilo.add(cursiva);
+            //JRadioButtonMenuItem radio = new JRadioButtonMenuItem("Cursiva", new ImageIcon("../iconos/lucida.gif"));
+            //group.add(radio);
+
+            /*configuraMenu("18", "tamano", " ", 9, 18, "");
             configuraMenu("25", "tamano", " ", 9, 25, "");// solo tiene que modificar el tamano.
             configuraMenu("30", "tamano", " ", 9, 30, "");//  el 9 e nada
-            configuraMenu("35", "tamano", " ", 9, 35, "");//  el 9 e nada
+            configuraMenu("35", "tamano", " ", 9, 35, "");//  el 9 e nada*/
 
+            creaTamanoJRadioButtonMenuItem("18", "../iconos/cortar.gif", 18 );
+            creaTamanoJRadioButtonMenuItem("25", "../iconos/cortar.gif", 25 );
+            creaTamanoJRadioButtonMenuItem("30", "../iconos/cortar.gif", 30 );
+            creaTamanoJRadioButtonMenuItem("35", "../iconos/cortar.gif", 35 );
         } 
+        // 
+        //         /**
+        //          * @param rotulo   ----nombre de subclase del elemento de la barra
+        //          * @param menu    -----nombre del elemento de la barra
+        //          * @param tipo_letra   - Nombre del tipo de letra que se asigna al texto.
+        //          * @param est      - Estilo que toma cada tipo de letra, negrita, cursiva etc
+        //          * @param tam       - Tamano de la letra. 
+        //          */
+        //         public void configuraMenu(String rotulo, String menu, String tipo_letra, int est, int tam, String icon){
+        //             z = tam;
+        //             JMenuItem elemento_menu = new JMenuItem(rotulo, new ImageIcon(icon));
+        // 
+        //             // -- 'elemento_menu' SE ASINA A LA PESTANA DE LA BARRA ESPECIFICADA EN EL 2º PARAMETRO.
+        //             if(menu == "fuent"){
+        //                 fuent.add(elemento_menu);
+        //                 if(tipo_letra == "Lucida Handwriting"){
+        //                     elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("adfaa", "Lucida Handwriting"));
+        //                 }else if(tipo_letra == "Arial"){
+        //                     elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("gagaga", "Arial"));
+        //                 }else if(tipo_letra == "Bell MT"){
+        //                     elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("gagaga", "Bell MT"));
+        //                 }
+        //             }/*else if(menu == "stilo"){
+        //             stilo.add(elemento_menu);             
+        //             if(est == Font.BOLD){                    
+        //             elemento_menu.addActionListener(new StyledEditorKit.BoldAction());
+        //             }
+        //             else if(est == Font.ITALIC){
+        //             elemento_menu.addActionListener(new StyledEditorKit.ItalicAction());
+        //             }                
+        //             }else if(menu == "tamano"){
+        //             tamano.add(elemento_menu);
+        //             }
+        //             elemento_menu.addActionListener(new  StyledEditorKit.FontSizeAction("adfaa", z));*/
+        // 
+        //         }
 
-        /**
-         * @param rotulo   ----nombre de subclase del elemento de la barra
-         * @param menu    -----nombre del elemento de la barra
-         * @param tipo_letra   - Nombre del tipo de letra que se asigna al texto.
-         * @param est      - Estilo que toma cada tipo de letra, negrita, cursiva etc
-         * @param tam       - Tamano de la letra. 
-         */
-        public void configuraMenu(String rotulo, String menu, String tipo_letra, int est, int tam, String icon){
+        public void creaTamanoJRadioButtonMenuItem(String text, String icono, int tam){
             z = tam;
-            JMenuItem elemento_menu = new JMenuItem(rotulo, new ImageIcon(icon));
-            // -- 'elemento_menu' SE ASINA A LA PESTANA DE LA BARRA ESPECIFICADA EN EL 2º PARAMETRO.
-            if(menu == "fuent"){
-                fuent.add(elemento_menu);
-                if(tipo_letra == "Lucida Handwriting"){
-                    elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("adfaa", "Lucida Handwriting"));
-                }else if(tipo_letra == "Arial"){
-                    elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("gagaga", "Arial"));
-                }else if(tipo_letra == "Bell MT"){
-                    elemento_menu.addActionListener(new StyledEditorKit.FontFamilyAction("gagaga", "Bell MT"));
-                }
-            }else if(menu == "stilo"){
-                stilo.add(elemento_menu);             
-                if(est == Font.BOLD){                    
-                    elemento_menu.addActionListener(new StyledEditorKit.BoldAction());
-                }
-                else if(est == Font.ITALIC){
-                    elemento_menu.addActionListener(new StyledEditorKit.ItalicAction());
-                }                
-            }else if(menu == "tamano"){
-                tamano.add(elemento_menu);
-            }
-            elemento_menu.addActionListener(new  StyledEditorKit.FontSizeAction("adfaa", z));
+            radio = new JRadioButtonMenuItem(text, new ImageIcon(icono));
+            radio.addActionListener(new  StyledEditorKit.FontSizeAction("adfaa", z));
+            tamano.add(radio);
+            group.add(radio);
+        }
+
+        public void creaFuenteJRadioButtonMenuItem(String TipoLetra, String icono){
+            radio = new JRadioButtonMenuItem(TipoLetra, new ImageIcon(icono));
+            radio.addActionListener(new StyledEditorKit.FontFamilyAction("adfaa", TipoLetra));
+            fuent.add(radio);
+            group.add(radio);
         }
     }
+
 }
